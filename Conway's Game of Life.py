8605eb6e30d3924a42de6cc22ad49f68e6a74cdf
Stas_Krasnagir <stas220693@gmail.com>
2020-11-10 19:55:27 +0300

    10-11-2020

--- Conway's Game of Life.py
+++ Conway's Game of Life.py
@@ -1,17 +1,25 @@
-def game_of_life(height, length):
+def create_universe(height, length):
     grid = [[0] * length] * height
-    for i in range(len(length)):
-        for j in range(len(height)):
+    return grid
+
+
+length = 5
+height = 10
+
+
+def game_step(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j] == 0:
-                res_for_dead = grid[i-1][j-1] + grid[i-1][j-1] + grid[i-1][j+1] + \
-                      grid[i][j-1] + grid[i][j+1] + \
-                      grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1]
+                res_for_dead = validate(grid[i - 1][j - 1]) + validate(grid[i - 1][j]) + validate(grid[i - 1][j + 1]) + \
+                               validate(grid[i][j - 1]) + validate(grid[i][j + 1]) + \
+                               validate(grid[i + 1][j - 1]) + validate(grid[i + 1][j]) + validate(grid[i + 1][j + 1])
                 if res_for_dead == 3:
                     grid[i][j] = 1
             if grid[i][j] == 1:
-                res_for_life = grid[i-1][j-1] + grid[i-1][j-1] + grid[i-1][j+1] + \
-                      grid[i][j-1] + grid[i][j+1] + \
-                      grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1]
+                res_for_life = validate(grid[i - 1][j - 1]) + validate(grid[i - 1][j]) + validate(grid[i - 1][j + 1]) + \
+                               validate(grid[i][j - 1]) + validate(grid[i][j + 1]) + \
+                               validate(grid[i + 1][j - 1]) + validate(grid[i + 1][j]) + validate(grid[i + 1][j + 1])
                 if res_for_life == 2 or res_for_life == 3:
                     grid[i][j] = 1
                 else:
@@ -19,6 +27,23 @@ def game_of_life(height, length):
     return grid
 
 
-length = 5
-height = 10
-print(game_of_life(height, length))
+grid = [[0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0]]
+row = len(grid)
+column = len(grid[0])
+
+def validate(grid):
+    if grid < 0:
+        res = 0
+        return res
+    else:
+        return grid
+
+print(game_step(grid))
+
+
+#and i > 0 and i < len(grid) and j > 0 and j < len(grid[0])
+#grid[i - 1][j - 1] + grid[i - 1][j] + grid[i - 1][j + 1] + grid[i][j - 1] + grid[i][j + 1] + grid[i + 1][j - 1] + grid[i + 1][j] + grid[i + 1][j + 1]
