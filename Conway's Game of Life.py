471ea97b08ba297378183f90c0a3caa0cb4ddae1
Stas_Krasnagir <stas220693@gmail.com>
2020-11-13 17:18:02 +0300

    13-11-20

--- Conway's Game of Life.py
+++ Conway's Game of Life.py
@@ -1,26 +1,40 @@
+import random, time
+
+
 def create_universe(height, length):
     grid = [[0] * length] * height
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            grid[i][j] = random.randrange(2)
     return grid
 
 
 length = 5
-height = 10
+height = 5
+#grid = create_universe(height, length)
+
+grid = [[0, 1, 0, 1, 0, 0],
+        [0, 1, 0, 1, 1, 0],
+        [0, 0, 1, 1, 1, 1],
+        [0, 1, 1, 1, 0, 0],
+        [1, 1, 0, 1, 1, 0],
+        [0, 0, 0, 1, 0, 1]]
 
 
 def game_step(grid):
     new_grid = grid.copy()
     for i in range(len(new_grid)):
         for j in range(len(new_grid[0])):
-            a = validate(i-1, j-1, new_grid)
-            b = validate(i-1, j, new_grid)
-            c = validate(i-1, j, new_grid)
+            a = validate(i - 1, j - 1, new_grid)
+            b = validate(i - 1, j, new_grid)
+            c = validate(i - 1, j, new_grid)
 
-            d = validate(i, j-1, new_grid)
-            e = validate(i, j+1, new_grid)
+            d = validate(i, j - 1, new_grid)
+            e = validate(i, j + 1, new_grid)
 
-            f = validate(i+1, j-1, new_grid)
-            g = validate(i+1, j, new_grid)
-            h = validate(i+1, j+1, new_grid)
+            f = validate(i + 1, j - 1, new_grid)
+            g = validate(i + 1, j, new_grid)
+            h = validate(i + 1, j + 1, new_grid)
 
             if new_grid[i][j] == 0:
                 res_for_dead = a + b + c + d + e + f + g + h
@@ -35,13 +49,6 @@ def game_step(grid):
     return new_grid
 
 
-grid = [[0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0],
-        [0, 1, 1, 0, 0],
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0]]
-
-
 def validate(i, j, new_grid):
     if 0 < i < len(new_grid) and 0 < j < len(new_grid[0]):
         return new_grid[i][j]
@@ -49,4 +56,12 @@ def validate(i, j, new_grid):
         res = 0
         return res
 
-print(game_step(grid))
+
+print(grid)
+time.sleep(1)
+count = 0
+while count < 5:
+    print(game_step(grid))
+    time.sleep(1)
+    grid = game_step(grid)
+    count += 1
